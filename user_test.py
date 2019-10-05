0347692184ba043e9991dfe9d459c6e4aed470e4
brian23-eng <b.odhiambo.bo@gmail.com>
2019-10-05 20:18:12 +0300

    add test for checking if initializing the objects is proper

--- user_test.py
+++ user_test.py
@@ -16,8 +16,27 @@ class TestUser(unittest.TestCase):
         '''
         self.new_user = User('Brian', 'Omondi', 'brian__jibril', 'steph')
 
+    def test_init(self):
+
+        '''
+        Test case to check if initialization of objects are proper
+
+        '''
+
+        self.assertEqual(self.new_user.first_name, 'Brian')
+
+        self.assertEqual(self.new_user.last_name, 'Omondi')
+
+        self.assertEqual(self.new_user.username, 'brian__jibril')
+        
+        self.assertEqual(self.new_user.password, 'steph')
+
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
+
 
 
