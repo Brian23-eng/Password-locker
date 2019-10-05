6b6726dfe0835096c86bae50f0592f77273dacf5
brian23-eng <b.odhiambo.bo@gmail.com>
2019-10-05 15:17:08 +0300

    add setup and teardown methods to run before each test and return the app to its default state after the test is run respectively

--- credential_test.py
+++ credential_test.py
@@ -2,3 +2,25 @@ import unittest #Importing the unnittest module
 from credentials import Credential #Importing the credentials class
 
 class TestCredential(unittest.TestCase):
+
+    '''
+    Args:
+        unittest.TestCase class that helps in creating test cases.
+
+    '''
+
+    def setUp(self):
+
+        '''
+        Set up method to run before each test cases
+
+        '''
+        self.new_cred = Credential('twitter', 'brian__jibril', 'steph')
+
+    def tearDown(self):
+
+        '''
+        Method that does clean up after each test has run.
+
+        '''
+        Credential.cred_list = []
