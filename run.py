f0b58af3db3f2ba84b9e8bb749a5a11e959e03bd
brian23-eng <b.odhiambo.bo@gmail.com>
2019-10-06 14:26:43 +0300

    create a function that generates a new password

--- run.py
+++ run.py
@@ -2,3 +2,16 @@ import unittest
 from user import User
 from credentials import Credential
 import random
+
+def register_user(f_name, l_name, u_name, password):
+
+    '''
+    Function that creates a new user
+
+    '''
+
+    new_user = User(f_name, l_name, u_name, password)
+
+    new_user.save_user()
+
+    
