3c1adc2ad425796c5cc83a49ea90fce7df511c92
brian23-eng <b.odhiambo.bo@gmail.com>
2019-10-06 14:42:31 +0300

    create function that logs in users

--- run.py
+++ run.py
@@ -14,4 +14,33 @@ def register_user(f_name, l_name, u_name, password):
 
     new_user.save_user()
 
-    
+def login_user():
+   
+    '''
+    Function that logs in the user to Password locker
+
+    '''
+    username = input("Enter your username \n")
+
+    password = input("Enter your password \n")
+
+    if User.user.exists(username):
+        for user in User.user_list:
+            if username == user.username and password == user.password:
+                return True
+            
+            else:
+                print("Invalid username or password! Please try again")
+
+                #login_user()
+
+    else:
+        print("User does not exist")
+        clear = input("Kindly press any key to continue...")
+        return False
+
+if __name__ == '__main__':
+    # main()
+
+
+
